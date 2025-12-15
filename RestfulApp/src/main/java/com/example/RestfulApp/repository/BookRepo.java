de1bd997fc7f5301597954279db07385eed19524
Dheeraj1997-java <k.durgadheerajkumar.4@gmail.com>
2025-12-16 00:36:18 +0530

    Add files via upload

---
+++ RestfulApp/src/main/java/com/example/RestfulApp/repository/BookRepo.java
@@ -0,0 +1,10 @@
+package com.example.RestfulApp.repository;
+
+import com.example.RestfulApp.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
+
+}
